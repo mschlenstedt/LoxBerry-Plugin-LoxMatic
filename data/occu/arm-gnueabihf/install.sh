db7fe5f80fd5d2ba42021fc38400b8e182d02085
Michael Schlenstedt <michael@schlenn.net>
2018-12-28 20:09:21 +0100

    OCCU V2.29

---
+++ data/occu/arm-gnueabihf/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p /opt/hm
+cp -R packages/lighttpd/* /opt/hm
+cp -R packages-eQ-3/HS485D/* /opt/hm
+cp -R packages-eQ-3/RFD/* /opt/hm
+cp -R packages-eQ-3/WebUI/* /opt/hm
+cp -R packages-eQ-3/LinuxBasis/* /opt/hm
