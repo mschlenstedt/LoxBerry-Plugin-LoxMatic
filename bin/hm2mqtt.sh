da2214cd5919b1ea730d625f775d443fa3dc86e6
Michael Schlenstedt <michael@schlenn.net>
2018-12-30 08:07:04 +0100

    MQTT COnfig

--- bin/hm2mqtt.sh
+++ bin/hm2mqtt.sh
@@ -24,10 +24,19 @@ LOGEND
 # Loglevel
 DEBUG=$(jq -r '.Debug' REPLACELBPCONFIGDIR/loxmatic.json)
 if [ "$DEBUG" = "true" ] || [ "$DEBUG" = "1" ]; then
-	LEVEL="0"
+	LEVEL="debug"
 else
-	LEVEL="2"
+	LEVEL="info"
+fi
+
+# MQTT Parameters
+BROKER=$(jq -r '.Broker' REPLACELBPCONFIGDIR/loxmatic.json)
+NAMES=$(jq -r '.Names' REPLACELBPCONFIGDIR/loxmatic.json)
+if [ -e $NAMES ]; then
+	$JSONNAME="-j $NAMES"
+else
+	$JSONNAME=""
 fi
 
 # Start HM2MQTT
-REPLACELBPDATADIR/hm2mqtt/index.js /dev/null 2>&1
+REPLACELBPDATADIR/hm2mqtt/index.js -d -m mqtt://$BROKER -a 127.0.0.1 -v $DEBUG $JSONNAME > REPLACELBPLOGDIR/hm2mqtt.log 2>&1 &
