812934f44b9cdda5640181ac62fc40a97592ce29
Michael Schlenstedt <Michael@schlenn.net>
2019-02-06 05:18:07 +0100

    Start RFD via hm_env

--- bin/rfd.sh
+++ bin/rfd.sh
@@ -44,5 +44,25 @@ else
 	LEVEL="2"
 fi
 
+# Patch config
+# only modify if real rf module found
+if [[ "${HM_HMRF_DEV}" == "HM-MOD-RPI-PCB" ]] ||
+   [[ "${HM_HMRF_DEV}" == "RPI-RF-MOD" ]]; then
+	# make sure [Interface 0] is uncommented
+	sed -i -e '/^#\[Interface 0\]/,/^#\s*$/ s/^#//' REPLACELBPCONFIGDIR/rfd.conf
+
+	# patch some settings to match what this hardware expects.
+	sed -i 's|^Type = .*$|Type = CCU2|' REPLACELBPCONFIGDIR/rfd.conf
+	sed -i 's|^ComPortFile = /dev/.*$|ComPortFile = /dev/mmd_bidcos|' REPLACELBPCONFIGDIR/rfd.conf
+	sed -i 's|^#*AccessFile = /dev/.*$|AccessFile = /dev/null|' REPLACELBPCONFIGDIR/rfd.conf
+	sed -i 's|^#*ResetFile = /dev/.*$|ResetFile = /dev/null|' REPLACELBPCONFIGDIR/rfd.conf
+	if ! grep -q "Improved Coprocessor Initialization" REPLACELBPCONFIGDIR/rfd.conf ; then
+		sed -i 's/\[Interface 0\]/Improved\ Coprocessor\ Initialization\ =\ true\n\n&/' REPLACELBPCONFIGDIR/rfd.conf
+	fi
+else
+	# otherwise disable the whole [Interface 0] part
+	sed -i -e '/^\[Interface 0\]/,/^\s*$/ s/^/#/' REPLACELBPCONFIGDIR/rfd.conf
+fi
+
 # Start RFD
 $HM_HOME/bin/rfd -d -l $LEVEL -f REPLACELBPCONFIGDIR/rfd.conf > /dev/null 2>&1
